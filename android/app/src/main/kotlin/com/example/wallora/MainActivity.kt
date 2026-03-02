2a3d7c369289f9c020628a1b82b39e7d67dda696
HexaGhost <hexaghost-09@tutamail.com>
2026-03-02 18:30:58 +0530

    New

--- android/app/src/main/kotlin/com/example/wallora/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wallora/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.wallora
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Aligns the Flutter view vertically with the window to allow edge-to-edge transparent navigation
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+    }
+}
